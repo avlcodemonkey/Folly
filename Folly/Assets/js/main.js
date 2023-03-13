6273bd38f6c02ace197954855609c2f380e0b1d4
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2023-03-13 17:17:20 -0400

    Set up HTMX to process table content after render.

--- Folly/Assets/js/main.js
+++ Folly/Assets/js/main.js
@@ -1,6 +1,20 @@
-﻿import "htmx.org/dist/htmx.js";
+﻿import "htmx.org";
 import "./tiny-components.js";
+
+window.htmx = require('htmx.org');
 
 document.getElementById('sidebar-button').addEventListener('click', () => {
     document.body.classList.toggle('sidebar-toggled');
+});
+
+// listen for updates to lit-table and process the table content
+document.addEventListener('lit-table-updated', (e) => {
+    if (!(e?.target?.shadowRoot && e?.detail)) {
+        return;
+    }
+
+    const target = e.target.shadowRoot.getElementById(e.detail);
+    if (target) {
+        window.htmx.process(target);
+    }
 });

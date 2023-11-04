29f9728e9699fa2bd44e0d5277deca50d864b546
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2023-11-04 15:43:09 -0400

    Change label and primary button for copy role to be clearer.

--- Folly.Web/Models/CopyRole.cs
+++ Folly.Web/Models/CopyRole.cs
@@ -4,6 +4,7 @@ using Folly.Resources;
 namespace Folly.Models;
 
 public sealed record CopyRole : BaseModel {
+    [Display(ResourceType = typeof(Roles), Name = nameof(Roles.Name))]
     [Required(ErrorMessageResourceType = typeof(Roles), ErrorMessageResourceName = nameof(Roles.ErrorNameRequired))]
     [StringLength(100, ErrorMessageResourceType = typeof(Core), ErrorMessageResourceName = nameof(Core.ErrorMaxLength))]
     public string Prompt { get; init; } = "";

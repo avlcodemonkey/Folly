26cd37d363092425454fece7544a9ffead40c320
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2023-11-11 17:46:52 -0500

    Start creating tests for web project.

---
+++ Folly.Web.Tests/Extensions/Services/RoleServiceExtensionsTests.cs
@@ -0,0 +1,68 @@
+using Folly.Domain.Models;
+using Folly.Extensions.Services;
+
+namespace Folly.Web.Tests.Extensions.Services;
+
+public class RoleServiceExtensionsTests {
+
+    public RoleServiceExtensionsTests() { }
+
+    [Fact]
+    public void SelectSingleAsDTO_ReturnsProjectedDTO() {
+        // arrange
+        var rolePermission = new RolePermission { Id = 1, RoleId = 2, PermissionId = 3 };
+        var role = new Role { Id = 4, Name = "test role", IsDefault = true, RolePermissions = new List<RolePermission> { rolePermission } };
+        var roles = new List<Role> { role }.AsQueryable();
+
+        // act
+        var dtos = roles.SelectAsDTO();
+
+        // assert
+        Assert.NotNull(dtos);
+        Assert.Single(dtos);
+
+        Assert.All(dtos, x => Assert.Equal(role.Id, x.Id));
+        Assert.All(dtos, x => Assert.Equal(role.Name, x.Name));
+        Assert.All(dtos, x => Assert.Equal(role.IsDefault, x.IsDefault));
+        Assert.All(dtos, x => Assert.NotNull(x.PermissionIds));
+        Assert.All(dtos, x => Assert.Single(x.PermissionIds!));
+        Assert.All(dtos, x => Assert.Contains(rolePermission.PermissionId, x.PermissionIds!));
+    }
+
+    [Fact]
+    public void SelectMultipleAsDTO_ReturnsProjectedDTOs() {
+        // arrange
+        var rolePermission1 = new RolePermission { Id = 1, RoleId = 2, PermissionId = 3 };
+        var role1 = new Role { Id = 4, Name = "test role 1", IsDefault = true, RolePermissions = new List<RolePermission> { rolePermission1 } };
+        var rolePermission2 = new RolePermission { Id = 5, RoleId = 6, PermissionId = 7 };
+        var role2 = new Role { Id = 6, Name = "test role 2", IsDefault = false, RolePermissions = new List<RolePermission> { rolePermission2 } };
+        var roles = new List<Role> { role1, role2 }.AsQueryable();
+
+        // act
+        var dtos = roles.SelectAsDTO().ToList();
+
+        // assert
+        Assert.NotNull(dtos);
+        Assert.Equal(2, dtos.Count);
+
+        Assert.Collection(dtos,
+            x => Assert.Equal(role1.Id, x.Id),
+            x => Assert.Equal(role2.Id, x.Id)
+        );
+        Assert.Collection(dtos,
+            x => Assert.Equal(role1.Name, x.Name),
+            x => Assert.Equal(role2.Name, x.Name)
+        );
+        Assert.Collection(dtos,
+            x => Assert.Equal(role1.IsDefault, x.IsDefault),
+            x => Assert.Equal(role2.IsDefault, x.IsDefault)
+        );
+        Assert.All(dtos, x => Assert.NotNull(x.PermissionIds));
+        Assert.All(dtos, x => Assert.Single(x.PermissionIds!));
+
+        Assert.Collection(dtos,
+            x => Assert.Contains(rolePermission1.PermissionId, x.PermissionIds!),
+            x => Assert.Contains(rolePermission2.PermissionId, x.PermissionIds!)
+        );
+    }
+}
---
+++ Folly.Web.Tests/Services/DatabaseFixture.cs
@@ -0,0 +1,73 @@
+using System.Security.Principal;
+using Folly.Domain;
+using Folly.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Folly.Web.Tests.Services;
+
+/// <summary>
+/// See https://learn.microsoft.com/en-us/ef/core/testing/testing-with-the-database
+/// for more details about using a database fixture for testing with xUnit.
+/// </summary>
+public class DatabaseFixture : IDisposable {
+    private const string _ConnectionString = "Filename=:memory:";
+    private readonly SqliteConnection _Connection;
+    private readonly Mock<IConfiguration> _MockConfiguration;
+    private static readonly object _Lock = new();
+    private static bool _DatabaseInitialized;
+
+    private static IHttpContextAccessor CreateHttpContextAccessor(User? user = null) {
+        var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+        if (user != null) {
+            mockHttpContextAccessor.Setup(x => x.HttpContext!.User.Identity).Returns(new GenericIdentity(user.UserName, "test"));
+        }
+        return mockHttpContextAccessor.Object;
+    }
+
+    public DatabaseFixture() {
+        _MockConfiguration = new Mock<IConfiguration>();
+        _MockConfiguration.Setup(x => x.GetSection("App").GetSection("Database")["FilePath"]).Returns(_ConnectionString);
+
+        // creates the SQLite in-memory database, which will persist until the connection is closed at the end of the test (see Dispose below).
+        _Connection = new SqliteConnection("Filename=:memory:");
+        _Connection.Open();
+
+        // lock allows us to use this fixture safely with multiple classes of tests if needed
+        lock (_Lock) {
+            if (!_DatabaseInitialized) {
+                // create the schema and user data we will need for each test
+                using (var dbContext = CreateContext()) {
+                    dbContext.Database.Migrate();
+                    dbContext.Users.Add(UserForCreate);
+                    dbContext.Users.Add(UserForUpdate);
+                    dbContext.Users.Add(UserForDelete);
+                    dbContext.SaveChanges();
+                }
+
+                _DatabaseInitialized = true;
+            }
+        }
+    }
+
+    public FollyDbContext CreateContext(User? user = null)
+        => new(new DbContextOptionsBuilder<FollyDbContext>().UseSqlite(_Connection).Options, _MockConfiguration.Object, CreateHttpContextAccessor(user));
+
+    public FollyDbContext CreateContextForCreate() => CreateContext(UserForCreate);
+
+    public FollyDbContext CreateContextForUpdate() => CreateContext(UserForUpdate);
+
+    public FollyDbContext CreateContextForDelete() => CreateContext(UserForDelete);
+
+    public User UserForCreate { get; } = new() { Id = -1, UserName = "create_user", Email = "create_user@fake.com", LanguageId = -1, FirstName = "Create" };
+    public User UserForUpdate { get; } = new() { Id = -2, UserName = "update_user", Email = "update_user@fake.com", LanguageId = -1, FirstName = "Update" };
+    public User UserForDelete { get; } = new() { Id = -3, UserName = "delete_user", Email = "delete_user@fake.com", LanguageId = -1, FirstName = "Delete" };
+
+    public void Dispose() {
+        _Connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}

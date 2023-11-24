e670c86196c22a9e24565dc02984445c90894ce2
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2023-11-24 14:26:09 -0500

    Add focus trapping for dialog. Finish up jsdoc comments.

--- Folly.Web/Assets/js/eventHandlers/onHtmxConfirm.js
+++ Folly.Web/Assets/js/eventHandlers/onHtmxConfirm.js
@@ -1,3 +1,87 @@
+// @ts-check
+
+/**
+ * @typedef TrackedListener
+ * @type {object}
+ * @property {string} type Event type.
+ * @property {EventListener} listener Callback function for event.
+ */
+
+/**
+ * Tracks added event listeners so they can be cleaned up.
+ * @type { TrackedListener[] }
+ */
+const documentEventListeners = [];
+
+/**
+ * Add event tracked listener to the document.
+ * @param {string} type Event type
+ * @param {EventListener} listener Callback function
+ */
+const addDocumentEventListener = (type, listener) => {
+    document.addEventListener(type, listener);
+    documentEventListeners.push({ type, listener });
+};
+
+/**
+ * Remove all tracked document event listeners.
+ */
+const removeDocumentEventListeners = () => {
+    let eventListenerReference;
+    // eslint-disable-next-line no-cond-assign
+    while ((eventListenerReference = documentEventListeners.shift())) {
+        document.removeEventListener(eventListenerReference.type, eventListenerReference.listener);
+        eventListenerReference = undefined;
+    }
+};
+
+/**
+ * Traps focus within the dialog element.
+ * @param {HTMLElement} dialog Dialog to trab focus for.
+ */
+const registerTabTrap = (dialog) => {
+    /** @type {HTMLElement[]} */
+    const tabbableElements = Array.from(dialog.querySelectorAll('a, button, input'))
+        .filter((tabbableElement) => {
+            const computedStyle = window.getComputedStyle(tabbableElement);
+            if (computedStyle.display === 'none' || computedStyle.visibility === 'hidden') {
+                return false;
+            }
+            if (['button', 'input'].includes(tabbableElement.tagName) && tabbableElement.disabled === true) {
+                return false;
+            }
+            return true;
+        });
+
+    if (tabbableElements.length === 0) {
+        return;
+    }
+
+    const firstTabbableElement = tabbableElements[0];
+    const lastTabbableElement = tabbableElements[tabbableElements.length - 1];
+
+    addDocumentEventListener('focusin', (event) => {
+        if (!dialog.contains(event.target)) {
+            event.preventDefault();
+            firstTabbableElement.focus();
+        }
+    });
+
+    firstTabbableElement.addEventListener('keydown', (event) => {
+        if (event.key === 'Tab' && event.shiftKey) {
+            event.preventDefault();
+            lastTabbableElement.focus();
+        }
+    });
+
+    lastTabbableElement.addEventListener('keydown', (event) => {
+        if (event.key === 'Tab' && !event.shiftKey) {
+            event.preventDefault();
+            firstTabbableElement.focus();
+        }
+    });
+};
+
 /**
  * Add custom behavior to htmx:confirm event to enable alert/confirm dialogs.
  * @param {CustomEvent} event
@@ -24,11 +108,14 @@ const onHtmxConfirm = async (event) => {
         cancelBtn.innerHTML = elt.getAttribute('hx-confirm-cancel');
 
         const closeHandler = () => {
+            removeDocumentEventListeners();
             dialog.removeEventListener('close', closeHandler);
             if (dialog.returnValue === 'ok') {
                 event.detail.issueRequest();
             }
         };
+
+        registerTabTrap(dialog);
         dialog.addEventListener('close', closeHandler);
 
         dialog.showModal();
@@ -49,9 +136,12 @@ const onHtmxConfirm = async (event) => {
         okBtn.innerHTML = elt.getAttribute('hx-alert-ok');
 
         const closeHandler = () => {
+            removeDocumentEventListeners();
             dialog.removeEventListener('close', closeHandler);
             elt.focus();
         };
+
+        registerTabTrap(dialog);
         dialog.addEventListener('close', closeHandler);
 
         dialog.showModal();

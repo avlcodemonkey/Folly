096a5668e14746f464f95275c0e29af877ccb8c3
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2023-12-09 17:55:37 -0500

    Eeasily enable htmx logging when needed for debugging.

--- Folly.Web/Assets/js/htmxConfig.js
+++ Folly.Web/Assets/js/htmxConfig.js
@@ -8,3 +8,8 @@ htmx.config.selfRequestsOnly = true;
 htmx.config.allowEval = false;
 htmx.config.historyCacheSize = 0;
 
+// enable logging for htmx
+if (document.body.getAttribute('data-environment') === 'development') {
+    // uncomment this line to enable detailed htmx logging
+    // htmx.logAll();
+}

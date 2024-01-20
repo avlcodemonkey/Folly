50b6dd1e2a4a87d67f262a7c8bd6b596e7520d6c
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2024-01-20 16:34:01 -0500

    Get pretty print for json working.

--- Folly.Web/Assets/js/components/jsonFormatter.js
+++ Folly.Web/Assets/js/components/jsonFormatter.js
@@ -1,30 +1,4 @@
-import { format } from 'fecha';
-
-// https://stackoverflow.com/a/7220510
-function syntaxHighlight(json) {
-    let formattedJson = json;
-    if (typeof formattedJson !== 'string') {
-        formattedJson = JSON.stringify(json, undefined, 2);
-    }
-
-    formattedJson = formattedJson.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-
-    return formattedJson.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match) => {
-        let cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return `<span class="${cls}">${match}</span>`;
-    });
-}
+import { prettyPrintJson } from 'pretty-print-json';
 
 /**
  * Web component for displaying json in a pretty way.
@@ -37,7 +11,14 @@ class JsonFormatter extends HTMLElement {
         super();
 
         try {
-            this.innerHTML = `<pre>${syntaxHighlight(this.textContent)}</pre>`;
+            const json = JSON.parse(this.textContent.length ? this.textContent : '{}');
+            const html = prettyPrintJson.toHtml(json, {
+                indent: 4,
+                linkUrls: false,
+                quoteKeys: true,
+                trailingCommas: false,
+            });
+            this.innerHTML = `<pre class="json-container">${html}</pre>`;
         } catch { /* empty */ }
     }
 }

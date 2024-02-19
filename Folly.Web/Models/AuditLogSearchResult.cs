3ced199eddb27f526f1a7c8bdd3c0db39722102a
avlcodemonkey <avlcodemonkey@users.noreply.github.com>
2024-02-19 17:47:34 -0500

    Remove unneeded comments.

--- Folly.Web/Models/AuditLogSearchResult.cs
+++ Folly.Web/Models/AuditLogSearchResult.cs
@@ -6,14 +6,10 @@ namespace Folly.Models;
 public sealed record AuditLogSearchResult : BaseModel {
     public new long Id { get; init; }
 
-    // @todo add Display attributes to use when showing these
     public Guid BatchId { get; init; }
 
     public string Entity { get; init; } = "";
 
-    /// <summary>
-    /// Descriptive value for the EntityState.
-    /// </summary>
     public string State { get; init; } = "";
 
     public string UniversalDate { get; init; } = "";
